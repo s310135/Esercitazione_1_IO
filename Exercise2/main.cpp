573c949b06f7a77aa61a561b772d0e8165b1003d
s310135 <s310135@studenti.polito.it>
2025-03-28 10:57:41 +0100

    commentato main

--- Exercise2/main.cpp
+++ Exercise2/main.cpp
@@ -1,54 +1,56 @@
-#include <iostream>
+#include <iostream>					//includo le librerie
 #include <fstream>
-#include <iomanip> //includo le librerie
+#include <iomanip> 					
 
-double valore;
-double somma=0;
-double min = 1.0;
+double valore; 						//è il valore mappato nella funzione
+double somma=0;						//è la somma di n valori
+double min = 1.0;	
 double max = 5.0;
-double intervallomin = -1.0;
-double intervallomax = 2.0;
-double media=0;
-int n=1;
+double intervallomin = -1.0;		//è l'intervallo in cui devo mappare i nuovi valori
+double intervallomax = 2.0;			//è l'intervallo in cui devo mappare i nuovi valori
+double media=0;						//è la media di n valori
+int n=1;							//è il numero intero che utilizzo per fare i calcoli
 int main()
 {
-	std::ifstream file("data.txt");
-	std::ofstream soluzione("result.txt", std::ofstream::trunc);  //se non esiste lo crea
-	soluzione << "# \N Mean\n";
+	std::ifstream file("data.txt");									//apro il file
+	std::ofstream soluzione("result.txt", std::ofstream::trunc);  	//apro il file result, se non esiste lo crea
+	soluzione << "# \N Mean\n";										//scrivo la prima riga con le "definizioni"
 	
-	if (file.fail())
+	if (file.fail())												//se non riesce ad aprire il file la risposta è true quindi esegue l'if
 	{
         std::cerr << "Il file non esiste" << std::endl;
-		return 1;
+		return 1;													//cioè c'è un errore
 	}
-    else
+    else															//è riuscito ad aprire il file
 		std::cerr << "Ho trovato il file!" << std::endl;
 	
-	std::string riga;
-	while(std::getline(file, riga))
+	std::string riga;												//crea la variabile riga
+	while(std::getline(file, riga))									//legge una riga del file alla volta finchè le righe non finiscono
 	{	
-		double numero =std::stod(riga);
+		double numero =std::stod(riga);								//trasforma la stringa in un numero double
 		if (numero == min){
-			std::cout << "il valore " << numero << std::setprecision(16) <<std::scientific << " corrisponde a: " << intervallomin << std::endl;
+			std::cout << "il valore " << numero << std::setprecision(16) << std::scientific << " corrisponde a: " << intervallomin << std::endl;
 			valore = numero ;
+			//stampa il nuovo valore mappato con una precisione di macchina di 16 e la notazione scientifica
 		}else if (numero == max){
 			std::cout << "il valore " << numero << std::setprecision(16) << std::scientific <<" corrisponde a: " << intervallomax << std::endl;
 			valore = numero;
 		}else{
 			valore = (intervallomin + ((numero - min) * (intervallomax - intervallomin) / (max - min)));
+			//calcolo per mappare il nuovo valore in un intervallo
 			std::cout << "il valore " << numero << std::setprecision(16) <<std::scientific << " corrisponde a: " << valore << std::setprecision(16)<<std::scientific << std::endl;
 		}
-		somma = valore + somma;
+		somma = valore + somma;										//dove somma a destra è il valore precedente che viene poi aggiornato
 		media = (somma)/n;
-		soluzione << n << " "  << std::setprecision(16) << std::scientific << media << std::endl;
+		soluzione << n << " "  << std::setprecision(16) << std::scientific << media << std::endl;			//stampo nel file il nuovo valore
 		//std::cout << n << " " << std::setprecision(16) << std::scientific << media  << std::endl;
 
-		n=n+1;
+		n=n+1;				//aggiorno il valore di n
 	}
 	
 	   
 	soluzione.close();
 			
-	file.close();
-    return 0;
+	file.close();				//chiudo i file
+    return 0;					//se non ci sono errori mi restituisce il valore 0, altrimenti 1
 }
